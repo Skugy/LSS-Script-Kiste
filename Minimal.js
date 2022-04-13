1008931e2afb2131e2b080e58be305c236bbf2e8
Skugy <103605607+Skugy@users.noreply.github.com>
2022-04-13 22:28:36 +0200

    Update Minimal.js

--- Minimal.js
+++ Minimal.js
@@ -1,11 +1,12 @@
 // ==UserScript==
-// @name         minimal
+// @name         Minimal
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Entfernt alles was Unwichtig ist
+// @author       Skugy
 // @match        https://www.leitstellenspiel.de/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=leitstellenspiel.de
+// @downloadURL  https://github.com/Skugy/LSS-Script-Kiste/raw/main/Minimal.js
 // @grant        none
 // ==/UserScript==
 
